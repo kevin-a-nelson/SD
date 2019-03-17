6024ba87921f854eed0f066b8e4bc147d1598629
Kevin Nelson <nelson67@stolaf.edu>
2019-03-16 21:49:23 -0500

    lab4B: zeroRed2.cpp, doubleGreen2.cpp, greyScale2.cpp complete

---
+++ images/doubleGreen2.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+ struct Pixel{
+     unsigned char red;
+     unsigned char green;
+     unsigned char blue;
+ };
+
+ void write_ppm(char *format_name, int x, int y, int range,unsigned char *data)
+{
+    cout.write((char *)data, 3 * x * y);
+}
+
+int main() {
+
+    char format_name[5];
+    int x, y, range;
+
+    cin >> format_name;
+    cin >> x >> y >> range;
+    cin.get();
+
+    // format_name = P6
+    // x = 506
+    // y = 336
+
+    Pixel**image = new Pixel*[y];
+
+    for(int i = 0; i < y; i++){
+        image[i] = new Pixel[x];
+    }
+    
+    for(int i = 0; i < y; ++i){
+        cin.read( (char *) image[i], x*sizeof(Pixel) );
+    }
+
+    format_name[0] = 'P';
+    format_name[1] = '6';
+
+    cout << format_name << "\n";
+    cout << x << " " << y << endl;
+    cout << range << endl;
+
+    for(int i = 0; i < y; i++){
+        for(int j = 0;j<x;j++){
+            image[i][j].green *= 2;
+        }
+    }
+
+    for(int i = 0; i < y; i++){
+        cout.write( (char *) image[i], x*sizeof(Pixel) );
+    }
+
+
+
+
+
+
+
+    cout << "\n";
+
+
+    return 0;
+}
