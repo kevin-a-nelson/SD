069508c01c6e2b5de1b6d5c01b0dfb10f649c48f
Kevin Nelson <nelson67@stolaf.edu>
2019-03-18 14:54:35 -0500

    lab4:complete

---
+++ images/pixelstruct.cpp
@@ -0,0 +1,14 @@
+ #include<iostream>
+ using namespace std;
+
+ struct Pixel{
+     int red;
+     int green;
+     int blue;
+ }
+
+ int main() {
+     Pixel p1;
+     p1.red = p1.green = p1.blue = 17;
+     cout << "p1.green " << (int)p1.green << endl;
+ }
