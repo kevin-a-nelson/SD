9fcd45e47479581e4d4cab9b92b31fc21b151f62
Kevin Nelson <nelson67@stolaf.edu>
2019-03-21 14:03:26 -0500

    lab5C: complete

---
+++ images/reader.cpp
@@ -0,0 +1,16 @@
+#include<fstream>
+#include<iostream>
+using namespace std;
+
+int main(){
+    ifstream g("in.txt");
+
+    int i,j;
+    g >> i >> j;
+    g.close();
+
+    cout << "i is " << i << " and j is " << j << endl;
+
+
+    return 0;
+}
