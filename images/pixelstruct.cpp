67865d1016850a3478d49d8460170ed678238c2c
Kevin Nelson <nelson67@stolaf.edu>
2019-03-21 19:45:07 -0500

    lab4: complete

--- images/pixelstruct.cpp
+++ images/pixelstruct.cpp
@@ -5,10 +5,12 @@
      int red;
      int green;
      int blue;
- }
+ };
 
  int main() {
      Pixel p1;
      p1.red = p1.green = p1.blue = 17;
      cout << "p1.green " << (int)p1.green << endl;
+
+     return 0;
  }
