1cba46b82d5f856030d25e590abdd73a8537c2a8
Kevin Nelson <nelson67@stolaf.edu>
2019-03-26 19:16:05 -0500

    lab5E: complete

--- images/ImageDriver-5F.cpp
+++ images/ImageDriver-5F.cpp
@@ -5,9 +5,10 @@ int main(){
 
     Image colorful_img("newfile.ppm");
 
-	
+    // colorful_img.print();
     // colorful_img.zeroRed();
-    colorful_img.greyScale();
-
+    // colorful_img.greyScale();
+    // colorful_img.horizSqueeze();
+    colorful_img.addBorder();
     return 0;
 }
