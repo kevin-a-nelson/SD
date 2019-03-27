ffc639f2b76ef4d4a655cbd61105aed24d69c29a
Kevin Nelson <nelson67@stolaf.edu>
2019-03-26 19:24:38 -0500

    lab5B: complete

--- images/Image1-5B.cpp
+++ images/Image1-5B.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<string>
+#include<fstream>
 using namespace std;
 
  struct Pixel{
@@ -31,10 +33,29 @@ class Image
             data[i] = new Pixel[width];
        }
     }
+    Image(const char * name){
+        
+        ifstream g(name);
+
+        char format_name[3];
+        g >> format_name >> width >> height >> dept;
+        g.get();
+        data = new Pixel*[height];
+        for(int i = 0; i < height; i++){
+            data[i] = new Pixel[width];
+        }
+
+        for(int i = 0; i < height; ++i){
+            g.read( (char *) data[i], width*sizeof(Pixel) );
+        }
+
+        g.close();
+
+    }
     int getWidth(){return width;}
     int getHeight(){return height;}
     void print(){
-        cout << "P6" << "\n" << width << " " << height << "\n" << dept << endl;
+        cout << "P6" << "\n" << width << " " << height << "\n" << dept << "\n";
         for(int i = 0; i < height; i++){
         cout.write( (char *) data[i], width*sizeof(Pixel) );
         }
@@ -42,7 +63,9 @@ class Image
 };
 int main(){
 
-    Image colorful_img(100,100);
+    // Image colorful_img(100,100);
+
+    Image colorful_img("newfile2.ppm");
 
 
     colorful_img.print();
